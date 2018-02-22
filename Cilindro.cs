0bb2831b804bf78d48adad8234ee5fdcb833424b
Urcera <aurceramartins@danielcastelao.org>
2018-02-22 16:58:58 +0000

    Exercicio1

---
+++ Cilindro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Cilindro
+{
+    public
+	public Cilindro()
+	{
+        string prova = "Esta e uma prova";
+        string prova1 = "Esta e uma prova";
+        string prova2 = @"Esta \e uma prova";
+
+        Console.WriteLine(prova);
+        Console.ReadKey();
+
+        prova += "nova provaaaaaaaaaaaa" + prova1;
+    }
+}
---
+++ cilindro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s = @"Nao e’ um codigo Unicode \u1234, nao e’ nova linha \n";
+            string s1 = "Hola sou uma string";
+
+            Console.WriteLine(s + s1);
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
